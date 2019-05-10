e6df193083509c6c2a732c7964b0cd2931bc2778
sheodox <imagamer27@msn.com>
2019-05-09 19:33:46 -0500

    world gen api shuffling, move texturing environment into worldgen

--- game.py
+++ game.py
@@ -4,8 +4,8 @@ from worldgen import WorldGen
 class Game:
     def __init__(self, main, level_name):
         self.main = main
-        self.world_gen = WorldGen(main)
-        self.level = self.world_gen.load(level_name)
+        self.world_gen = WorldGen(main, level_name)
+        self.level = self.world_gen.get_level()
 
     def spawn_character(self):
         pass
--- worldgen.py
+++ worldgen.py
@@ -1,24 +1,34 @@
+from panda3d.core import TextureStage, Texture
+
 from levelparser import LevelParser
-from texturer import Texturer
 
 
 class WorldGen:
-    def __init__(self, main):
+    def __init__(self, main, level_name):
         self.main = main
-        self.parser = LevelParser()
         self.main.camera.set_pos(7, -49, 5)
-        self.texturer = Texturer()
 
-    def load(self, level):
-        level = self.parser.load_level_file(level)
+        self.parser = LevelParser(level_name)
+        self.level = level = self.parser.load_level_file()
         for block in level.blocks:
             self._create_block(block['pos'], block['width'])
-        return level
+
+    def get_level(self):
+        return self.level
 
     def _create_block(self, pos, width):
         block = self.main.loader.loadModelCopy('models/block')
         block.set_pos(pos)
         block.set_sx(width)
         block.reparent_to(self.main.render)
-        self.texturer.texture_block(block, width, 'art/block.png')
+        self._texture_block(block, width, 'art/block.png')
+
+    def _texture_block(self, np, block_width, texture_path):
+        texture = base.loader.load_texture(texture_path)
+        ts = TextureStage('ts')
+
+        np.setTexScale(ts, block_width, 1)
+        np.set_texture(ts, texture, 1)
+        texture.setWrapU(Texture.WM_repeat)
+        texture.setWrapV(Texture.WM_repeat)
 
